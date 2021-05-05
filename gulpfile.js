b93adc390e2f102744b1a7a3869ac338666026d5
dmitry_susin <dsusin@bk.ru>
2021-05-05 19:39:54 +0300

    Исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,11 @@ const sync = require("browser-sync").create();
 const csso = require("postcss");
 const rename = require("gulp-rename");
 const htmlmin = require("gulp-htmlmin");
-const imagemin = require("gulp-image-min");
+const imagemin = require("gulp-imagemin");
 const cssmin = require("gulp-cssmin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -22,8 +25,8 @@ const styles = () => {
       autoprefixer(),
       csso()
     ]))
-    .pipe(rename("style.min.css"))
     .pipe(cssmin())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
@@ -56,16 +59,73 @@ const images = () => {
 
 exports.images = images;
 
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"))
+  }
+
+  exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+  }
+
+  exports.sprite = sprite;
+
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+  "source/fonts/*.{woff2,woff}",
+  "source/*.ico",
+  "source/img/**/*.{jpg,png,svg}",
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"))
+  done();
+  }
+
+  exports.copy = copy;
+
+// Del
+
+const clean = () => {
+  return del("build");
+  };
+
+exports.clean = clean;
+
+// Build
+
+const build = gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+  );
+
+exports.build = build;
+
 // Server
 
 const server = (done) => {
   sync.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
+  server: {
+  baseDir: "build"
+  },
+  cors: true,
+  notify: false,
+  ui: false,
   });
   done();
 }
@@ -75,8 +135,8 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html", gulp.series("html"));
 }
 
 exports.default = gulp.series(
